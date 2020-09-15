b497b9fb85b23598b73bf33340f3d2cff39ebebf
maksimboikodev <maksimboikodev@gmail.com>
2020-09-15 14:50:15 +0300

    new commit

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/kelseyhightower/envconfig" // не убирать и не комментировать, починить ошибку, добавив алиас
+	// не убирать и не комментировать, починить ошибку, добавив алиас
+	"github.com/kelseyhightower/envconfig"
 	"github.com/sirupsen/logrus"
 )
 

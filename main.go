7f2c810c1ec0eaa3c22731758fe0ac44bfd04b39
maksiboikodev <maksimboikodev@gmail.com>
2020-09-22 11:38:11 +0300

    clean code v1.2

--- main.go
+++ main.go
@@ -18,12 +18,15 @@ func main() {
 	var eConf EnvConfig
 	envconfig.Process("", &eConf)
 	file, _ := os.OpenFile(eConf.LogFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+
 	var log = logrus.New()
 	log.Out = file
+
 	var d float32
 	fmt.Print("Введите температуру: ")
 	fmt.Scan(&d)
 	fmt.Println(d)
+
 	c := Celsius(d)
 	log.Info("температура по фаренгейту  ", toFahrenheit(c))
 

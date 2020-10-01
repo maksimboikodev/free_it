d8f252c0d5ff3c9be8ace20d9debee0168012523
maksimboikodev <maksimboikodev@gmail.com>
2020-10-01 10:34:58 +0300

    v0.3

--- pkg/datatypes/ochered.go
+++ pkg/datatypes/ochered.go
@@ -1,8 +1,17 @@
 package datatypes
 
-import "fmt"
+import (
+	"fmt"
 
-func OcheredFIFO() {
+	"github.com/sirupsen/logrus"
+)
+
+type HistoryHander struct {
+	Samples []string
+	Logger  *logrus.Logger
+}
+
+func OcheredFIFO(h *HistoryHander) {
 	var queue []string
 	queue = append(queue, "Hello ")
 	queue = append(queue, "world!")
@@ -12,6 +21,9 @@ func OcheredFIFO() {
 		queue[0] = ""
 		queue = queue[1:]
 		fmt.Println(queue)
+
+		h.Samples = append(h.Samples, queue)
+		h.Logger.Info("очередь FIFO ", h.Samples, queue)
 	}
 }
 
@@ -21,15 +33,22 @@ type ListNode struct {
 }
 
 func OdnSpis() {
+
 	n1 := &ListNode{Val: 1}
 	n2 := &ListNode{Val: 2}
 	n1.Next = n2
 	n3 := &ListNode{Val: 3}
 	n2.Next = n3
-	fmt.Println(n1, n2, n3)
+	fmt.Println(n1.Val, n2.Val, n3.Val)
+	i := n1
+	for i != nil {
+		fmt.Println(i.Val)
+		i = i.Next
+	}
 
 }
 func Reverse(str string) (result string) {
+
 	for _, v := range str {
 		result = string(v) + result
 	}

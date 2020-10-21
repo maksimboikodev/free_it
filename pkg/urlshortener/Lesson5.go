3052d976ed4d900875146512aa111dc4f3cbb6a8
maksimboikodev <maksimboikodev@gmail.com>
2020-10-21 11:02:17 +0300

    v0.6

--- pkg/urlshortener/Lesson5.go
+++ pkg/urlshortener/Lesson5.go
@@ -13,68 +13,6 @@ func NewURLStore() *URLStore {
 	return urls
 }
 
-/*func (s *URLStore) Get(key string) string {
-
-	for key, value := range s.Urls {
-		fmt.Println("Ключ:", key, "Значение", value)
-
-		if _, ok := s.Urls["TUT"]; ok {
-			fmt.Println("Найдено", key, value, ok)
-		}
-
-		/*if key, ok := s.Urls["Key"]; ok {
-			fmt.Println(key, ok)
-		}
-	}
-	return key
-}*/
-
-/*func (s *URLStore) Set(key, url string) bool {
-	// ваш код
-}*/
-
-/*func (s *URLStore) Count() int {
-	i := 1
-	for key := range s.Urls {
-		fmt.Println(key)
-		i++
-	}
-	return i
-}*/
-/*func (s *URLStore) Count() int {
-	sum,ok := s.Urls{
-		fmt.Println(sum)
-	}
-
-	return len(sum)
-}*/
-/*func (s *URLStore) Get(key string) string {
-
-	for key, value := range s.Urls {
-		fmt.Println("Ключ:", key, "Значение", value)
-
-	}
-
-	key, ok := s.Urls["TUT"]
-	if ok {
-		fmt.Println("Найдено", key, ok)
-		return key
-	}
-	return key
-}*/
-
-/*func (s *URLStore) Get(key string) string {
-
-	for key, value := range s.Urls {
-		fmt.Println("Ключ:", key, "Значение", value)
-
-	}
-
-	if key, ok := s.Urls["TUT"]; ok {
-		fmt.Println(key, ok)
-	}
-	return key
-}*/
 func (s *URLStore) Get(key string) string {
 	var val string
 	_, ok := s.Urls["TUT"]
@@ -83,4 +21,16 @@ func (s *URLStore) Get(key string) string {
 
 	}
 	return val
+}
+func (s *URLStore) Set(key, url string) bool {
+	_, ok := s.Urls[key]
+	if ok {
+		fmt.Println("Значение существует")
+		return false
+	} else {
+		s.Urls[key] = url
+		fmt.Println("Значение добавлено")
+		return true
+
+	}
 }

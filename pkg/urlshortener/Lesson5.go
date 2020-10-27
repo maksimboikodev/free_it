ee09ea1676f3e5c93ebc417e70eda2bda7371bcd
maksimboikodev <maksimboikodev@gmail.com>
2020-10-27 13:27:02 +0300

    fix func Get,Set,Put

--- pkg/urlshortener/Lesson5.go
+++ pkg/urlshortener/Lesson5.go
@@ -1,7 +1,6 @@
 package urlshortener
 
 import (
-	"fmt"
 	"strconv"
 )
 
@@ -15,47 +14,31 @@ func NewURLStore() *URLStore {
 }
 
 func (s *URLStore) Get(key string) string {
-	if u, ok := s.Urls[key]; ok {
-		fmt.Println(" url of ", u)
-		return u
+	if _, ok := s.Urls[key]; !ok {
+		return "URL not found"
 	}
-	return ""
+	return s.Urls[key]
 }
 
 func (s *URLStore) Set(key, url string) bool {
-	bl := false
-	for _, value := range s.Urls {
-		if value == url {
-			bl = true
-		}
+	if _, ok := s.Urls[key]; ok {
+		return false
 	}
-	if !bl {
-		s.Urls[key] = url
-		return true
-	}
-	return false
-
+	s.Urls[key] = url
+	return true
 }
+
 func (s *URLStore) Put(url string) string {
-	k := genKey(len(s.Urls))
-	g := s.Set(k, url)
-	if g == true {
-		s.Urls[k] = url
-	}
-	fmt.Print(g)
-	fmt.Print(s.Urls)
-	return ""
+	key := genKey(s.Count())
+	s.Set(key, url)
+	return key
 }
 
 func (s *URLStore) Count() int {
 	return len(s.Urls)
 }
+
 func genKey(n int) string {
-	if n < 10 {
-		shortUrl := strconv.Itoa(n) + "_URL"
-		return shortUrl
-	}
-	shortUrl := strconv.Itoa(n) + "_urllll"
-	return shortUrl
+	return strconv.Itoa(n) + "_URL"
 
 }

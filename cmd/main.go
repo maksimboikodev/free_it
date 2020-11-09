06e7fe05bbe57bdb10f61ad8fa2ac5deb8319d7e
maksimboikodev <maksimboikodev@gmail.com>
2020-11-09 08:35:19 +0300

    working version of chanell package

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/kelseyhightower/envconfig"
 	"github.com/maksimboikodev/test/pkg/chanell"
+	"github.com/maksimboikodev/test/pkg/datatypes"
+	"github.com/maksimboikodev/test/pkg/urlshortener"
 	"github.com/maksimboikodev/test/pkg/usecases"
 	"github.com/sirupsen/logrus"
 )
@@ -22,39 +25,38 @@ func main() {
 	var log = logrus.New()
 	log.Out = file
 
-	/*var d float64
+	var d float64
 	fmt.Print("enter temperature: ")
 	fmt.Scan(&d)
-	fmt.Println(d)*/
+	fmt.Println(d)
 
 	tempHistoryHandler := &usecases.HistoryHander{
 		Logger:  log,
 		Samples: []usecases.Fahrenheit{},
 	}
 
-	//usecases.CheckAndSave(tempHistoryHandler, usecases.Celsius(d), 2)
+	usecases.CheckAndSave(tempHistoryHandler, usecases.Celsius(d), 2)
 
-	//datatypes.List(tempHistoryHandler)
-	//datatypes.TurnFIFO(tempHistoryHandler)
-	//str := "Golang"
-	//datatypes.Reverse(str, tempHistoryHandler)
-	//datatypes.ReverseString(str, tempHistoryHandler)
-	//storage := urlshortener.NewURLStore()
-	//tempHistoryHandler.Info("Key New:  ", storage.Set("3_Url", "abrakada.com"))
-	//tempHistoryHandler.Info("Key New:  ", storage.Set("5_Url", "ab.com"))
-	//tempHistoryHandler.Info("ALL MAP:  ", storage)
-	//tempHistoryHandler.Info("Url: ", storage.Get("12_URL"))
-	//tempHistoryHandler.Info("Url: ", storage.Get("5_Url"))
+	datatypes.List(tempHistoryHandler)
+	datatypes.TurnFIFO(tempHistoryHandler)
+	str := "Golang"
+	datatypes.Reverse(str, tempHistoryHandler)
+	datatypes.ReverseString(str, tempHistoryHandler)
+	storage := urlshortener.NewURLStore()
+	tempHistoryHandler.Info("Key New:  ", storage.Set("3_Url", "abrakada.com"))
+	tempHistoryHandler.Info("Key New:  ", storage.Set("5_Url", "ab.com"))
+	tempHistoryHandler.Info("ALL MAP:  ", storage)
+	tempHistoryHandler.Info("Url: ", storage.Get("12_URL"))
+	tempHistoryHandler.Info("Url: ", storage.Get("5_Url"))
 
-	/*if _, error := urlshortener.Sqrt(d); error != nil {
+	if _, error := urlshortener.Sqrt(d); error != nil {
 		tempHistoryHandler.Info("err: ", error)
-	}*/
+	}
 
 	ch := make(chan string)
 	go chanell.Read(ch)
 	inchen := "ABCDEF"
 	ch <- inchen
 	tempHistoryHandler.Info("The value at the entrance to the channel  ", inchen)
-	tempHistoryHandler.Info("The value at the entrance to the channel 2 ", ch)
 	tempHistoryHandler.Info("The value at the exit from the channel    ", chanell.Read)
 }

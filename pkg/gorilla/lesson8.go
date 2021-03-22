e207b28b8c5fca8111053646df3dd41a32654483
maksimboikodev <maksimboikodev@gmail.com>
2021-03-22 14:48:36 +0300

    added protection against adding new posts (using headers)

--- pkg/gorilla/lesson8.go
+++ pkg/gorilla/lesson8.go
@@ -31,10 +31,7 @@ type response struct {
 	Message string `json:"message,omitempty"`
 }
 
-//var Students []Freeit
-
 func createConnection() *sql.DB {
-
 	connStr := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 	db, err := sql.Open("postgres", connStr)
@@ -42,188 +39,102 @@ func createConnection() *sql.DB {
 	if err != nil {
 		panic(err)
 	}
-
-	// check the connection
 	err = db.Ping()
 
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Println("Successfully connected!")
-	// return the connection
 	return db
 }
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-
-	// create an empty user of type Freeit
-	var user Freeit
-
-	// decode the json request to user
-	err := json.NewDecoder(r.Body).Decode(&user)
-
-	if err != nil {
-		log.Fatalf("Unable to decode the request body.  %v", err)
+	hed := r.Header.Get("Password")
+	if hed == "Pass" {
+		var user Freeit
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			log.Fatalf("Unable to decode the request body.  %v", err)
+		}
+		insertID := insertUser(user)
+		res := response{
+			ID:      insertID,
+			Message: "User created successfully",
+		}
+		json.NewEncoder(w).Encode(res)
 	}
-
-	// call insert user function and pass the user
-	insertID := insertUser(user)
-
-	// format a response object
-	res := response{
-		ID:      insertID,
-		Message: "User created successfully",
-	}
-
-	// send the response
-	json.NewEncoder(w).Encode(res)
 }
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	// get the userid from the request params, key is "id"
 	params := mux.Vars(r)
-
-	// convert the id type from string to int
 	id, err := strconv.Atoi(params["id"])
-
 	if err != nil {
 		log.Fatalf("Unable to convert the string into int.  %v", err)
 	}
-
-	// call the getUser function with user id to retrieve a single user
 	user, err := getUser(int64(id))
-
 	if err != nil {
 		log.Fatalf("Unable to get user. %v", err)
 	}
-
-	// send the response
 	json.NewEncoder(w).Encode(user)
 }
 func GetAllUser(w http.ResponseWriter, r *http.Request) {
-	hed := r.Header.Get("Pass")
-	fmt.Print(hed)
-	// get all the users in the db
 	users, err := getAllUsers()
-
 	if err != nil {
 		log.Fatalf("Unable to get all user. %v", err)
 	}
-
-	// send all the users as response
 	json.NewEncoder(w).Encode(users)
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-
-	// get the userid from the request params, key is "id"
 	params := mux.Vars(r)
-
-	// convert the id type from string to int
 	id, err := strconv.Atoi(params["id"])
-
 	if err != nil {
 		log.Fatalf("Unable to convert the string into int.  %v", err)
 	}
-
-	// create an empty user of type models.User
 	var user Freeit
-
-	// decode the json request to user
 	err = json.NewDecoder(r.Body).Decode(&user)
-
 	if err != nil {
 		log.Fatalf("Unable to decode the request body.  %v", err)
 	}
-
-	// call update user to update the user
 	updatedRows := updateUser(int64(id), user)
-
-	// format the message string
 	msg := fmt.Sprintf("User updated successfully. Total rows/record affected %v", updatedRows)
-
-	// format the response message
 	res := response{
 		ID:      int64(id),
 		Message: msg,
 	}
-
-	// send the response
 	json.NewEncoder(w).Encode(res)
 }
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-
-	// get the userid from the request params, key is "id"
 	params := mux.Vars(r)
-
-	// convert the id in string to int
 	id, err := strconv.Atoi(params["id"])
-
 	if err != nil {
 		log.Fatalf("Unable to convert the string into int.  %v", err)
 	}
-
-	// call the deleteUser, convert the int to int64
 	deletedRows := deleteUser(int64(id))
-
-	// format the message string
 	msg := fmt.Sprintf("User updated successfully. Total rows/record affected %v", deletedRows)
-
-	// format the reponse message
 	res := response{
 		ID:      int64(id),
 		Message: msg,
 	}
-
-	// send the response
 	json.NewEncoder(w).Encode(res)
 }
 func insertUser(user Freeit) int64 {
-
-	// create the postgres db connection
 	db := createConnection()
-
-	// close the db connection
 	defer db.Close()
-
-	// create the insert sql query
-	// returning userid will return the id of the inserted user
 	sqlStatement := `INSERT INTO users (name, position, age) VALUES ($1, $2, $3) RETURNING userid`
-
-	// the inserted id will store in this id
 	var id int64
-
-	// execute the sql statement
-	// Scan function will save the insert id in the id
 	err := db.QueryRow(sqlStatement, user.Name, user.Position, user.Age).Scan(&id)
-
 	if err != nil {
 		log.Fatalf("Unable to execute the query. %v", err)
 	}
-
 	fmt.Printf("Inserted a single record %v", id)
-
-	// return the inserted id
 	return id
 }
 func getUser(id int64) (Freeit, error) {
-	// create the postgres db connection
 	db := createConnection()
-
-	// close the db connection
 	defer db.Close()
-
-	// create a user of models.User type
 	var user Freeit
-
-	// create the select sql query
 	sqlStatement := `SELECT * FROM users WHERE userid=$1`
-
-	// execute the sql statement
 	row := db.QueryRow(sqlStatement, id)
-
-	// unmarshal the row object to user
 	err := row.Scan(&user.ID, &user.Name, &user.Age, &user.Position)
-
 	switch err {
 	case sql.ErrNoRows:
 		fmt.Println("No rows were returned!")
@@ -234,105 +145,60 @@ func getUser(id int64) (Freeit, error) {
 		log.Fatalf("Unable to scan the row. %v", err)
 	}
 
-	// return empty user on error
 	return user, err
 }
 func getAllUsers() ([]Freeit, error) {
-	// create the postgres db connection
 	db := createConnection()
-
-	// close the db connection
 	defer db.Close()
-
 	var users []Freeit
-
-	// create the select sql query
 	sqlStatement := `SELECT * FROM users`
-
-	// execute the sql statement
 	rows, err := db.Query(sqlStatement)
-
 	if err != nil {
 		log.Fatalf("Unable to execute the query. %v", err)
 	}
-
-	// close the statement
 	defer rows.Close()
-
-	// iterate over the rows
 	for rows.Next() {
 		var user Freeit
-
-		// unmarshal the row object to user
 		err = rows.Scan(&user.ID, &user.Name, &user.Age, &user.Position)
-
 		if err != nil {
 			log.Fatalf("Unable to scan the row. %v", err)
 		}
-
-		// append the user in the users slice
 		users = append(users, user)
 
 	}
-
-	// return empty user on error
 	return users, err
 }
 
 func updateUser(id int64, user Freeit) int64 {
-
-	// create the postgres db connection
 	db := createConnection()
-
-	// close the db connection
 	defer db.Close()
 
-	// create the update sql query
 	sqlStatement := `UPDATE users SET name=$2, location=$3, age=$4 WHERE userid=$1`
-
-	// execute the sql statement
 	res, err := db.Exec(sqlStatement, id, user.Name, user.Position, user.Age)
 
 	if err != nil {
 		log.Fatalf("Unable to execute the query. %v", err)
 	}
 
-	// check how many rows affected
 	rowsAffected, err := res.RowsAffected()
-
 	if err != nil {
 		log.Fatalf("Error while checking the affected rows. %v", err)
 	}
-
 	fmt.Printf("Total rows/record affected %v", rowsAffected)
-
 	return rowsAffected
 }
 func deleteUser(id int64) int64 {
-
-	// create the postgres db connection
 	db := createConnection()
-
-	// close the db connection
 	defer db.Close()
-
-	// create the delete sql query
 	sqlStatement := `DELETE FROM users WHERE userid=$1`
-
-	// execute the sql statement
 	res, err := db.Exec(sqlStatement, id)
-
 	if err != nil {
 		log.Fatalf("Unable to execute the query. %v", err)
 	}
-
-	// check how many rows affected
 	rowsAffected, err := res.RowsAffected()
-
 	if err != nil {
 		log.Fatalf("Error while checking the affected rows. %v", err)
 	}
-
 	fmt.Printf("Total rows/record affected %v", rowsAffected)
 
 	return rowsAffected
@@ -341,16 +207,19 @@ func deleteUser(id int64) int64 {
 func Router() *mux.Router {
 
 	router := mux.NewRouter()
-	router.HandleFunc("/api/user/{id}", GetUser).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/user", GetAllUser).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/newuser", CreateUser).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/user/{id}", UpdateUser).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/api/deleteuser/{id}", DeleteUser).Methods("DELETE", "OPTIONS")
-	router.HandleFunc("/api/user/queryline", QueryLine).Methods("GET")
+	router.HandleFunc("/api/user/{id}", GetUser).Methods("GET")
+	router.HandleFunc("/api/user", GetAllUser).Methods("GET")
+	router.HandleFunc("/api/newuser", CreateUser).Methods("POST").Headers("Password", "Pass")
+	router.HandleFunc("/api/user/{id}", UpdateUser).Methods("PUT")
+	router.HandleFunc("/api/deleteuser/{id}", DeleteUser).Methods("DELETE")
+	router.HandleFunc("/api/queryline", QueryLine).Methods("GET")
 
 	return router
 }
+
+//query response http://localhost:8080/api/queryline?id=1
 func QueryLine(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	fmt.Fprintf(w, "Name: %s", name)
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+	user, _ := getUser(int64(id))
+	json.NewEncoder(w).Encode(user)
 }
